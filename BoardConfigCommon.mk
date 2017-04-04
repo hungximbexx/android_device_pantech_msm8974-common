e9dcc1a85e2edea75c1e7171aef7395ad0e1917b
Chau Truong Thinh <chautruongthinh@gmail.com>
2017-04-04 15:08:20 +0700

    msm8974-common: Sepolicy: Update sepolicy, now enforcing
    Change-Id: I5e4c8401e1fd8479c5903357d3288f08cf663ce6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -39,7 +39,7 @@ TARGET_CPU_VARIANT := krait
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=NULL,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 vmalloc=260M loglevel=0 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=NULL,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 vmalloc=260M loglevel=0
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
@@ -71,6 +71,8 @@ QCOM_BT_USE_SMD_TTY := true
 TARGET_USE_COMPAT_GRALLOC_ALIGN := true
 USE_DEVICE_SPECIFIC_CAMERA := true
 TARGET_HAS_LEGACY_CAMERA_HAL1 := true
+TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS:= true
+
 
 # Charger
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
@@ -166,8 +168,8 @@ TARGET_NO_RPC := true
 # SELinux
  include device/qcom/sepolicy/sepolicy.mk
 
-# BOARD_SEPOLICY_DIRS += \
-    $(PLATFORM_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += \
+    $(COMMON_PATH)/sepolicy
 
 # Sensor Compat
 BOARD_GLOBAL_CFLAGS += -DCOMPAT_SENSORS_M
