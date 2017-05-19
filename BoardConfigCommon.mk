730f3faeb196e8d9cac45e8a1d75bb4acedd8f60
chautruongthinh <chautruongthinh@gmail.com>
2017-05-19 10:24:36 +0700

    msm8974: Add Healthd light paths

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -77,7 +77,10 @@ TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS:= true
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_SHOW_PERCENTAGE := true
-BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
+BACKLIGHT_PATH := "/sys/class/leds/lcd-backlight/brightness"
+RED_LED_PATH := "/sys/class/leds/led:rgb_red/brightness"
+GREEN_LED_PATH := "/sys/class/leds/led:rgb_green/brightness"
+BLUE_LED_PATH := "/sys/class/leds/led:rgb_blue/brightness"
 
 # CM Hardware
 BOARD_HARDWARE_CLASS += $(COMMON_PATH)/cmhw
